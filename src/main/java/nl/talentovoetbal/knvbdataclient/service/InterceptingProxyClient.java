67b51c8fc24424d60c9dbdcbe11fa971d65a747c
aranarunakiri <aran.arunakiri@gmail.com>
2016-03-02 13:38:07 +0100

    proxyclient for multi client support

--- src/main/java/nl/talentovoetbal/knvbdataclient/service/InterceptingProxyClient.java
+++ src/main/java/nl/talentovoetbal/knvbdataclient/service/InterceptingProxyClient.java
@@ -1,28 +1,35 @@
 package nl.talentovoetbal.knvbdataclient.service;
 
-
 import retrofit.appengine.UrlFetchClient;
+import retrofit.client.Client;
 import retrofit.client.Request;
 import retrofit.client.Response;
 
 import java.io.IOException;
 
-public class InterceptingUrlFetchClient extends UrlFetchClient {
+public class InterceptingProxyClient implements retrofit.client.Client {
+
     public interface Interceptor {
         void beforeExecute(Request request);
     }
 
     private Interceptor interceptor;
+    private Client realClient;
 
     @Override
     public Response execute(Request request) throws IOException {
         if (interceptor != null) {
             interceptor.beforeExecute(request);
         }
-        return super.execute(request);
+        return realClient.execute(request);
+    }
+
+    public void setRealClient(Client realClient) {
+        this.realClient = realClient;
     }
 
     public void setInterceptor(Interceptor interceptor) {
         this.interceptor = interceptor;
     }
+
 }
--- src/main/java/nl/talentovoetbal/knvbdataclient/service/KNVBDataServiceFactory.java
+++ src/main/java/nl/talentovoetbal/knvbdataclient/service/KNVBDataServiceFactory.java
@@ -1,24 +1,11 @@
 package nl.talentovoetbal.knvbdataclient.service;
 
 
-import com.jakewharton.retrofit.Ok3Client;
-import com.sun.javafx.fxml.builder.URLBuilder;
 import io.mikael.urlbuilder.UrlBuilder;
 import nl.talentovoetbal.knvbdataclient.Main;
-import okhttp3.*;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.internal.http.HttpMethod;
-import okhttp3.logging.HttpLoggingInterceptor;
 import org.apache.commons.codec.digest.DigestUtils;
-import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
-import retrofit.appengine.UrlFetchClient;
-import retrofit.client.*;
-
-import javax.print.URIException;
-import java.io.IOException;
-import java.util.Map;
+import retrofit.client.Client;
 
 public class KNVBDataServiceFactory {
     public static final String BASE_URL = "http://api.knvbdataservice.nl/api";
@@ -26,10 +13,11 @@ public class KNVBDataServiceFactory {
     public static final String PARAM_PATH_NAME = "path_name";
     private static KNVBDataService service;
 
-    public static KNVBDataService get(final String pathName, final String apiKey) {
+    public static KNVBDataService get(final String pathName, final String apiKey, final Client httpClient) {
         if (service == null) {
-            InterceptingUrlFetchClient client = new InterceptingUrlFetchClient();
-            client.setInterceptor(new InterceptingUrlFetchClient.Interceptor() {
+            InterceptingProxyClient client = new InterceptingProxyClient();
+            client.setRealClient(httpClient);
+            client.setInterceptor(new InterceptingProxyClient.Interceptor() {
                 @Override
                 public void beforeExecute(retrofit.client.Request request) {
                     String path = request.getUrl().toString();
