b3cc627b9395d7b26df1cfc873beda6b3968dc0a
aranarunakiri <aran.arunakiri@gmail.com>
2016-03-02 13:44:46 +0100

    proxyclient for multi client support

--- src/main/java/nl/talentovoetbal/knvbdataclient/MainTest.java
+++ src/main/java/nl/talentovoetbal/knvbdataclient/MainTest.java
@@ -1,18 +1,22 @@
 package nl.talentovoetbal.knvbdataclient;
 
+import com.jakewharton.retrofit.Ok3Client;
 import nl.talentovoetbal.knvbdataclient.model.GenericResponse;
 import nl.talentovoetbal.knvbdataclient.model.Team;
 import nl.talentovoetbal.knvbdataclient.service.KNVBDataService;
 import nl.talentovoetbal.knvbdataclient.service.KNVBDataServiceFactory;
+import okhttp3.OkHttpClient;
 
 public class MainTest {
     public static final String API_KEY = "PlNHZdEj6p3kBb3";
     public static final String PATH_NAME = "voorwaartst";
 
     public static void main(String[] args) {
-        KNVBDataService service = KNVBDataServiceFactory.get(PATH_NAME, API_KEY);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Ok3Client ok3Client = new Ok3Client(okHttpClient);
+        KNVBDataService service = KNVBDataServiceFactory.get(PATH_NAME, API_KEY, ok3Client);
         GenericResponse<Team> teamResponseCall = service.getTeams();
-        Main.logger.info("teamsize = "+teamResponseCall.data.size());
+        Main.logger.info("teamsize = " + teamResponseCall.data.size());
 
         for (Team team : teamResponseCall.data) {
             System.out.println(team.teamName);
